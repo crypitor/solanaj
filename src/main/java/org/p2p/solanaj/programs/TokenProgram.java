fed2ead48c6d19efc020b431c2e887cc0119a14e
trongcauta <trongcauhcmus@gmail.com>
2022-02-09 12:50:00 +0700

    update TokenProgram. create associated account and find associated token address

--- src/main/java/org/p2p/solanaj/programs/TokenProgram.java
+++ src/main/java/org/p2p/solanaj/programs/TokenProgram.java
@@ -7,6 +7,7 @@ import org.p2p.solanaj.core.TransactionInstruction;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ public class TokenProgram extends Program {
 
     public static final PublicKey PROGRAM_ID = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
     private static final PublicKey SYSVAR_RENT_PUBKEY = new PublicKey("SysvarRent111111111111111111111111111111111");
+    private static final PublicKey ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
 
     private static final int INITIALIZE_METHOD_ID = 1;
     private static final int TRANSFER_METHOD_ID = 3;
@@ -25,18 +27,19 @@ public class TokenProgram extends Program {
     /**
      * Transfers an SPL token from the owner's source account to destination account.
      * Destination pubkey must be the Token Account (created by token mint), and not the main SOL address.
-     * @param source SPL token wallet funding this transaction
+     *
+     * @param source      SPL token wallet funding this transaction
      * @param destination Destined SPL token wallet
-     * @param amount 64 bit amount of tokens to send
-     * @param owner account/private key signing this transaction
+     * @param amount      64 bit amount of tokens to send
+     * @param owner       account/private key signing this transaction
      * @return transaction id for explorer
      */
     public static TransactionInstruction transfer(PublicKey source, PublicKey destination, long amount, PublicKey owner) {
         final List<AccountMeta> keys = new ArrayList<>();
 
-        keys.add(new AccountMeta(source,false, true));
-        keys.add(new AccountMeta(destination,false, true));
-        keys.add(new AccountMeta(owner,true, false));
+        keys.add(new AccountMeta(source, false, true));
+        keys.add(new AccountMeta(destination, false, true));
+        keys.add(new AccountMeta(owner, true, false));
 
         byte[] transactionData = encodeTransferTokenInstructionData(
                 amount
@@ -52,11 +55,11 @@ public class TokenProgram extends Program {
     public static TransactionInstruction transferChecked(PublicKey source, PublicKey destination, long amount, byte decimals, PublicKey owner, PublicKey tokenMint) {
         final List<AccountMeta> keys = new ArrayList<>();
 
-        keys.add(new AccountMeta(source,false, true));
+        keys.add(new AccountMeta(source, false, true));
         // index 1 = token mint (https://docs.rs/spl-token/3.1.0/spl_token/instruction/enum.TokenInstruction.html#variant.TransferChecked)
         keys.add(new AccountMeta(tokenMint, false, false));
-        keys.add(new AccountMeta(destination,false, true));
-        keys.add(new AccountMeta(owner,true, false));
+        keys.add(new AccountMeta(destination, false, true));
+        keys.add(new AccountMeta(owner, true, false));
 
         byte[] transactionData = encodeTransferCheckedTokenInstructionData(
                 amount,
@@ -73,10 +76,10 @@ public class TokenProgram extends Program {
     public static TransactionInstruction initializeAccount(final PublicKey account, final PublicKey mint, final PublicKey owner) {
         final List<AccountMeta> keys = new ArrayList<>();
 
-        keys.add(new AccountMeta(account,false, true));
+        keys.add(new AccountMeta(account, false, true));
         keys.add(new AccountMeta(mint, false, false));
-        keys.add(new AccountMeta(owner,false, true));
-        keys.add(new AccountMeta(SYSVAR_RENT_PUBKEY,false, false));
+        keys.add(new AccountMeta(owner, false, true));
+        keys.add(new AccountMeta(SYSVAR_RENT_PUBKEY, false, false));
 
         ByteBuffer buffer = ByteBuffer.allocate(1);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
@@ -92,9 +95,9 @@ public class TokenProgram extends Program {
     public static TransactionInstruction closeAccount(final PublicKey source, final PublicKey destination, final PublicKey owner) {
         final List<AccountMeta> keys = new ArrayList<>();
 
-        keys.add(new AccountMeta(source,false, true));
+        keys.add(new AccountMeta(source, false, true));
         keys.add(new AccountMeta(destination, false, true));
-        keys.add(new AccountMeta(owner,true, false));
+        keys.add(new AccountMeta(owner, true, false));
 
         ByteBuffer buffer = ByteBuffer.allocate(1);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
@@ -105,6 +108,51 @@ public class TokenProgram extends Program {
                 keys,
                 buffer.array()
         );
+    }
+
+    /**
+     * Create Associated account
+     *
+     * @param fundingAddress      signer and fee payer
+     * @param walletAddress       owner of Associated account
+     * @param splTokenMintAddress token mint address
+     * @return transaction instruction create Associated account
+     * @throws Exception
+     */
+    public static TransactionInstruction createAssociatedTokenAccount(final PublicKey fundingAddress, final PublicKey walletAddress, final PublicKey splTokenMintAddress) throws Exception {
+        PublicKey associatedTokenAddress = findAssociatedTokenAddress(walletAddress, splTokenMintAddress);
+        final List<AccountMeta> keys = new ArrayList<>();
+
+        keys.add(new AccountMeta(fundingAddress, true, true));
+        keys.add(new AccountMeta(associatedTokenAddress, false, true));
+        keys.add(new AccountMeta(walletAddress, false, false));
+        keys.add(new AccountMeta(splTokenMintAddress, false, false));
+        keys.add(new AccountMeta(SystemProgram.PROGRAM_ID, false, false));
+        keys.add(new AccountMeta(TokenProgram.PROGRAM_ID, false, false));
+        keys.add(new AccountMeta(TokenProgram.SYSVAR_RENT_PUBKEY, false, false));
+
+        ByteBuffer buffer = ByteBuffer.allocate(0);
+        return createTransactionInstruction(
+                ASSOCIATED_TOKEN_PROGRAM_ID,
+                keys,
+                buffer.array()
+        );
+    }
+
+    public static TransactionInstruction transferTokens(final PublicKey owner,
+                                                        final PublicKey sourcePublicKey,
+                                                        final PublicKey destinationPublicKey,
+                                                        long amount,
+//                                                        memo,
+                                                        final PublicKey mint,
+                                                        int decimals,
+                                                        boolean overrideDestinationCheck) {
+
+        return null;
+    }
+
+    public static PublicKey findAssociatedTokenAddress(final PublicKey walletAddress, final PublicKey tokenMintAddress) throws Exception {
+        return PublicKey.findProgramAddress(Arrays.asList(walletAddress.toByteArray(), PROGRAM_ID.toByteArray(), tokenMintAddress.toByteArray()), ASSOCIATED_TOKEN_PROGRAM_ID).getAddress();
     }
 
     private static byte[] encodeTransferTokenInstructionData(long amount) {
